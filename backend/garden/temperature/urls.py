362095d457e2c3956f2325888ad7fd035a3c04f9
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2025-03-23 14:52:20 +0700

    Add API endpoints for deleting oldest temperature

--- backend/garden/temperature/urls.py
+++ backend/garden/temperature/urls.py
@@ -21,11 +21,13 @@ from .views import (
     RetrieveTemperatureBoundView,
     RetrieveMostRecentTemperatureRecord,
     SyncMostRecentTemperatureRecord,
+    DeleteOldestTemperatureRecord,
 )
 
 urlpatterns = [
     path('bound/update/', UpdateTemperatureBoundView.as_view(), name='temperature_bound_update'),
     path('bound/get/', RetrieveTemperatureBoundView.as_view(), name='temperature_bound_retrieve'),
     path('record/get/', RetrieveMostRecentTemperatureRecord.as_view(), name='most_recent_temperature_retrieve'),
-    path('record/sync/', SyncMostRecentTemperatureRecord.as_view(), name='most_recent_temperature_sync')
+    path('record/sync/', SyncMostRecentTemperatureRecord.as_view(), name='most_recent_temperature_sync'),
+    path('record/delete/', DeleteOldestTemperatureRecord.as_view(), name='oldest_temperature_delete'),
 ]
