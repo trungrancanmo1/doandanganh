b0815730624a7dc1005f886b47f098f5caabd2d4
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2025-03-25 01:34:41 +0700

    Add API for uploading and getting plant images

--- backend/garden/pest/admin.py
+++ backend/garden/pest/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import PlantImage
 
 # Register your models here.
+admin.site.register(PlantImage)
--- backend/garden/pest/models.py
+++ backend/garden/pest/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+class PlantImage(models.Model):
+    timestamp = models.DateTimeField(null=False)
+    image = models.ImageField(upload_to='plant_images/')
+    user = models.ForeignKey(
+        to=get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='plant_image_of',
+    )
+    
+    class Meta:
+        ordering = ['user', '-timestamp']
+    
+    def __str__(self):
+        username = self.user.username
+        email = self.user.email
+        timestamp = self.timestamp
+        return f"{username} ({email}): image {self.id} at {timestamp}"
