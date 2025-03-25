34b1fc8aded6ed8219c99a6e1bc5d26bc19f3e99
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2025-03-25 22:25:42 +0700

    Add API for managing light control mode

--- backend/garden/light/admin.py
+++ backend/garden/light/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import LightBound, LightRecord
+from .models import LightBound, LightRecord, LightControlMode
 
 # Register your models here.
 admin.site.register(LightBound)
 admin.site.register(LightRecord)
+admin.site.register(LightControlMode)
