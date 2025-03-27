ebabd275089f59822ce396b7eb553717d113b886
zun <trungdunglebui17112004@gmail.com>
2025-03-27 14:27:58 +0700

    dung-using API for moisture instead of humidity

--- backend/garden/moisture/views.py
+++ backend/garden/moisture/views.py
@@ -12,6 +12,10 @@ from aio_helper.client import get_aio_client
 from aio_helper.feed import get_or_create_feed
 from aio_helper.data import get_unread_data_from_feed
 
+from utils import ifdb_client
+from garden.settings import INFLUXDB
+
+
 # Create your views here.
 
 class UpdateMoistureBoundView(generics.UpdateAPIView):
@@ -99,7 +103,30 @@ class RetrieveMostRecentMoistureRecord(views.APIView):
                 raise exceptions.ValidationError('n must be an integer')
             if n <= 0:
                 raise exceptions.ValidationError('n must be positive')
-        records = MoistureRecord.objects.filter(user=self.request.user).order_by('-timestamp')
+            
+        # records = MoistureRecord.objects.filter(user=self.request.user).order_by('-timestamp')
+        #===================================
+        # INFLUX DATABASE
+        #===================================
+        query = f'''
+        SELECT time as timestamp, value
+        FROM 'sensor_data'
+        WHERE type = 'humidity'
+        ORDER BY time DESC
+        LIMIT {int(n)}
+        '''
+
+        # if n is not None:
+        #     records = records[:n]
+
+        table = ifdb_client.query(query=query, database=INFLUXDB['bucket'])
+        data_frame = table.to_pandas()
+        data_list = data_frame.to_dict(orient='records')
+        #===================================
+        # INFLUX DATABASE
+        #===================================
+        
+        return Response(data_list, status=status.HTTP_200_OK)
         if n is not None:
             records = records[:n]
         serializer = MoistureRecordSerializer(records, many=True)
