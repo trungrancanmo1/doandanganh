004a376b73c12d2a172aed50c76ca9b21d458342
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2025-04-21 22:22:50 +0700

    Change email checker service

--- backend/garden/user/services.py
+++ backend/garden/user/services.py
@@ -3,7 +3,8 @@ from rest_framework import serializers
 from django.conf import settings
 
 
-def validate_email(email):
+# Deprecated: ZeroBounce is not available
+def _validate_email(email):
     url = settings.EMAIL_VALIDATION_API_URL
     params = {
         'api_key': settings.EMAIL_VALIDATION_API_KEY,
@@ -14,3 +15,20 @@ def validate_email(email):
         raise serializers.ValidationError(f'Failed to validate email with {url}')
     response = response.json()
     return response['status'] == 'valid'
+
+
+# Currently used with hunter.io
+def validate_email(email):
+    url = settings.EMAIL_VALIDATION_API_URL
+    params = {
+        'api_key': settings.EMAIL_VALIDATION_API_KEY,
+        'email': email,
+    }
+    response = requests.get(url, params).json()
+    data = response.get('data')
+    if not data:
+        raise serializers.ValidationError(f'Failed to validate email with {url}')
+    status = data.get('status')
+    if not status:
+        raise serializers.ValidationError(f'Failed to validate email with {url}')
+    return status == 'valid'
